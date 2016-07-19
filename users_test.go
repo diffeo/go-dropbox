6243bfe95db7deff7d49fbc686937f7a58b1cd4c
Kevin Doyle <kevin@meta.sc>
2016-07-19 00:38:37 -0400

    add support for get account batch

--- users_test.go
+++ users_test.go
@@ -11,3 +11,19 @@ func TestUsers_GetCurrentAccount(t *testing.T) {
 	_, err := c.Users.GetCurrentAccount()
 	assert.NoError(t, err)
 }
+
+func TestUsers_GetAccountBatch(t *testing.T) {
+	c := client()
+	setup, err := c.Users.GetCurrentAccount()
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, setup.AccountID, "account id required for test setup")
+
+	out, err := c.Users.GetAccountBatch(&GetAccountBatchInput{
+		AccountIDs: []string{setup.AccountID},
+	})
+
+	assert.NoError(t, err, "getting account batch")
+	assert.NotEmpty(t, out, "account batch output")
+	assert.EqualValues(t, setup.AccountID, out[0].AccountID, "requested match retrieved")
+}
