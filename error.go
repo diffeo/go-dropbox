2c1b7045afa0340f93b5e765675158ceee4ab697
kujenga <ataylor0123@gmail.com>
2016-07-29 11:23:47 -0400

    expanded error type with tests

--- error.go
+++ error.go
@@ -1,13 +1,45 @@
 package dropbox
 
+import (
+	"net/http"
+)
+
+// error tag constant values
+const (
+	TooManyRequests = "too_many_requests"
+)
+
 // Error response.
 type Error struct {
 	Status     string
 	StatusCode int
-	Summary    string `json:"error_summary"`
+	Header     http.Header
+	Summary    string                 `json:"error_summary"`
+	Message    string                 `json:"user_message"` // optionally present
+	Err        map[string]interface{} `json:"error"`
 }
 
 // Error string.
 func (e *Error) Error() string {
 	return e.Summary
+}
+
+// Tag returns the inner tag for the error
+func (e *Error) Tag() (tag, value string) {
+	var ok bool
+	tag, ok = e.Err[".tag"].(string)
+	if !ok {
+		return
+	}
+
+	data, ok := e.Err[tag].(map[string]interface{})
+	if !ok {
+		return
+	}
+
+	value, ok = data[".tag"].(string)
+	if !ok {
+		return
+	}
+	return
 }
---
+++ error_test.go
@@ -0,0 +1,26 @@
+package dropbox
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestError(t *testing.T) {
+	c := client()
+	out, err := c.Files.GetMetadata(&GetMetadataInput{
+		Path: "/this/does/not/exist.txt",
+	})
+
+	assert.Nil(t, out, "no object should be returned on error")
+	assert.Error(t, err, "error should occur for nonexistant path")
+
+	derr, ok := err.(*Error)
+
+	assert.True(t, ok, "error should be a package error")
+	assert.NotNil(t, derr.Header, "error headers should be present")
+	assert.NotNil(t, derr.Err, "error detail should be present")
+	tag, value := derr.Tag()
+	assert.Equal(t, "path", tag, "error should indicate the path was invalid")
+	assert.Equal(t, "not_found", value, "error should indicate not found")
+}
